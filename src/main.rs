63791a839a9ccf77ccaf21358ad9cfa1e1e0e0a7
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2018-02-26 10:53:49 +0200

    templates and assets

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,32 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+extern crate rocket_contrib;
 
+use rocket_contrib::Template;
+use rocket::response::NamedFile;
 
-mod routes {
-    #[get("/")]
-    pub fn index() -> &'static str {
-        "Hello, world!"
-    }
+use std::collections::HashMap;
+use std::path::{Path, PathBuf};
+
+#[get("/")]
+pub fn index() -> Template {
+    let mut context = HashMap::new();
+    context.insert("name", "Rust");
+    Template::render("index", &context)
+}
+
+#[get("/<asset..>")]
+fn assets(asset: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("/home/oleg/projects/bbb_webui/src/assets").join(asset)).ok()
+}
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite()
+        .mount("/", routes![index, assets])
+        .attach(Template::fairing())
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![routes::index]).launch();
+    rocket().launch();
 }
-
-//fn assets_path() -> &'static str {
-//    if cfg!(debug_assertions) {
-//        "./src/assets/"
-//    } else {
-//        "./assets/"
-//    }
-//}
